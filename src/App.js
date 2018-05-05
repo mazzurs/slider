b63b1ac0560dfbcdad0adbd1a1b55b3aaee2b19f
Sergey Mazur <mazzurs@ya.ru>
2018-05-05 17:48:55 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react'
-// import {Provider} from 'react-redux'
 import logo from './logo.svg'
 import './App.css'
 import Slider from './components/slider/slider'
 import feed from './feed.json'
-
-import animals from './img/animals.jpeg'
-import people from './img/people.jpeg'
-import sepia from './img/sepia.jpeg'
-import sepia1 from './img/sepia1.jpeg'
-import sepia2 from './img/sepia2.jpeg'
-import tech from './img/tech.jpeg'
 
 class App extends Component {
   constructor (props) {
